0694d5502c2f8b3fd1ff7682e93580b8d159a801
HackZers <hackzers@yandex.ru>
2025-01-16 21:23:37 +0300

    Добавил метод проверки аргумента на нахождение в промежутке.

--- DS.Shared/Helpers/ThrowHelper.cs
+++ DS.Shared/Helpers/ThrowHelper.cs
@@ -48,4 +48,30 @@ public static class ThrowHelper
 
 		throw new ArgumentException("String cannot be empty or white space.", paramName);
 	}
+
+	/// <summary>
+	///     Проверяет, что аргумент находится в указанном промежутке.
+	/// </summary>
+	/// <param name="argument">Аргумент.</param>
+	/// <param name="startRange">Начало промежутка.</param>
+	/// <param name="endRange">Конец промежутка.</param>
+	/// <param name="paramName">Наименование аргумента.</param>
+	/// <exception cref="ArgumentNullException">Выбрасывается если аргумент равен null.</exception>
+	/// <exception cref="ArgumentException">Выбрасывается если аргумент пустой или содержит только пробелы.</exception>
+	public static void ArgumentOutOfRange<T>(
+		T argument,
+		T startRange,
+		T endRange,
+		[CallerArgumentExpression("argument")] string? paramName = null
+	) where T : IComparable
+	{
+		ArgumentNotNull(argument, paramName);
+		// -1 - текущее значение меньше переданного
+		// 0 - значение я равны
+		// 1 - текущее значение больше переданного
+		if (argument.CompareTo(startRange) == -1 || argument.CompareTo(endRange) == 1)
+		{
+			throw new ArgumentOutOfRangeException(paramName);
+		}
+	}
 }
